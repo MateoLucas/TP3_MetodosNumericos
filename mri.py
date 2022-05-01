63742a477269cdc84cd463a6228b8cc0b715f188
bytheseaside <102440878+bytheseaside@users.noreply.github.com>
2022-05-01 19:04:17 -0300

    Update mri.py

--- mri.py
+++ mri.py
@@ -8,21 +8,21 @@ tol = 1e-10 #tolerancia
 delta = 1e-2
 
 def solver(L,l,n):
+    # DEFINICIÓN DE FUNCIONES LAMBDA (f(x) Y f'(x))
     func = lambda r: (((r**2)*(n**2)*mu*np.pi/(l**2))*((((r**2)+(l**2))**0.5)-r))-L
-    #dato: bobina mas chica: NANO Tess -> l = 1mm = 1x10**-3 m. l no llega  aser tan chica como para que df de cero, 
+    #dato: bobina mas chica: NANO Tess -> l = 1mm = 1x10**-3 m. l no llega a ser tan chica como para que df de cero, 
     # o que se pueda confundir por cero. 
     dfunc = lambda r: (np.pi*mu*n**2*r*(((r**2+l**2)**0.5)-r)*(2*((r**2+l**2)**0.5)-r))/(l**2*((r**2+l**2)**0.5))
-    # Lo que buscamos: x_(k+1) = x_k - f(x_k)/df(x_k)
-                  #definimos r inicial
-    rk = 16*(10*3) * L/(n**2) 
+    
+    # Se busca realizar la iteración: x_(k+1) = x_k - f(x_k)/df(x_k)
+    rk = 16*(10*3) * L/(n**2) # Definimos r inicial (r_0)
 
-    if abs(dfunc(rk)) < tol:                #Si la derivada es "0", usamos el metodo de la secnate
+    if abs(dfunc(rk)) < tol: # Si la derivada es "0", usamos el metodo de la secnate
         div = (func(rk+delta)-func(rk))/delta
         j=2
-        while abs(div) < tol:           #En caso de que div siga siendo 0 agrandamos el intrervalo
+        while abs(div) < tol: #En caso de que div siga siendo 0 agrandamos el intervalo
              div = (func(rk+(j*delta))-func(rk))/(j*delta)
              j+=1
-
     else:
         div = dfunc(rk)
     rkm1 = rk - (func(rk)/div)
@@ -33,19 +33,20 @@ def solver(L,l,n):
 
 
 
-def graph():
-    #ploteo de valores
+def graph(): 
+    # Ploteo de r(L) L ∈ [1pH, 100uH] con l=0.2 m y N= 10,100,1000
     l= 0.2
     delta_L = 1*(10**-6)
-    Ls = np.arange(10**(-9),100*10**(-6), delta_L)
+    Ls = np.arange(10**(-9),100*10**(-6), delta_L) #Valores de L
     Ln=len(Ls)
-    rs = np.empty((Ln,3))
+    rs = np.empty((Ln,3)) #Se inicializa el array donde se guardaran los r(L)
+    
     for N in [10,100,1000]:         
         for i in range(Ln):
-            rs[i,int(np.log10(N)-1)]=solver(Ls[i],l,N)
+            rs[i,int(np.log10(N)-1)]=solver(Ls[i],l,N) #log10(N)-1 es para ubicar la columna correspondiente (0,1,2)
 
 
-    radio = plt.figure("Radio")
+    radio = plt.figure("Radio del inductor - l = 0.2 m")
     ax = radio.add_subplot(3,1,1)
     plt.xlabel("L[H]")
     plt.ylabel("r(L)[m] - N=10")
